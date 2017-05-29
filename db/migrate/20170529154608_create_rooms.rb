1330a21c561f69811562c14143d52b56ee8dd902
Phuc Nguyen <nhphuc412@gmail.com>
2017-05-29 22:49:21 +0700

    fix bug home have no call rooms

--- db/migrate/20170529154608_create_rooms.rb
+++ db/migrate/20170529154608_create_rooms.rb
@@ -1,13 +1,14 @@
 class CreateRooms < ActiveRecord::Migration[5.0]
   def change
     create_table :rooms do |t|
-      t.integer :room_type_id
+      t.integer :room_type_id, null: false
       t.integer :bed_type_id, null: false
       t.integer :bath_type_id, null: false
+      t.integer :home_id
       t.integer :accommodates, null: false
       t.text :description, null: false
       t.text :image, null: false
-      t.integer :price
+      t.integer :price, null: false
       t.string :room_name, null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170528170931) do
+ActiveRecord::Schema.define(version: 20170529154608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,13 +94,14 @@ ActiveRecord::Schema.define(version: 20170528170931) do
   end
 
   create_table "rooms", force: :cascade do |t|
-    t.integer  "room_type_id"
+    t.integer  "room_type_id", null: false
     t.integer  "bed_type_id",  null: false
     t.integer  "bath_type_id", null: false
+    t.integer  "home_id"
     t.integer  "accommodates", null: false
     t.text     "description",  null: false
     t.text     "image",        null: false
-    t.integer  "price"
+    t.integer  "price",        null: false
     t.string   "room_name",    null: false
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
